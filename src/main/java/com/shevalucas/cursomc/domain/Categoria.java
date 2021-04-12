cbce3386e8c383fb49f62a4d1ec3f18668c4050a
shevalucas <lucas.costa@sou.unifal-mg.edu.br>
2021-04-12 12:03:05 -0300

    Banco de dados H2 e criacao auto da BD

--- src/main/java/com/shevalucas/cursomc/domain/Categoria.java
+++ src/main/java/com/shevalucas/cursomc/domain/Categoria.java
@@ -1,12 +1,21 @@
 package com.shevalucas.cursomc.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    
     private  String nome;
 
     public Categoria(){
