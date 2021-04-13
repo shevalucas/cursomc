27f85e02e2f18856511ea586a5774cb88ff03943
shevalucas <lucas.costa@sou.unifal-mg.edu.br>
2021-04-13 16:21:59 -0300

    Atualizando uma categoria com PUT

--- src/main/java/com/shevalucas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/shevalucas/cursomc/resources/CategoriaResource.java
@@ -19,7 +19,7 @@ public class CategoriaResource {
     private CategoriaService service;
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> listar(@PathVariable Integer id){
+    public ResponseEntity<Categoria> find(@PathVariable Integer id){
         Categoria obj = service.find(id);
 
         return ResponseEntity.ok().body(obj);
@@ -27,10 +27,17 @@ public class CategoriaResource {
 
     @PostMapping
     public ResponseEntity <Void> insert(@RequestBody Categoria obj){
-        System.out.println("teste");
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Categoria obj,
+                                       @PathVariable Integer id) {
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
     }
 }
