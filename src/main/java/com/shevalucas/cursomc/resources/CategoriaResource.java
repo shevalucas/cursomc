d00a3518b9f1294f762577ed94c5129027725a10
shevalucas <lucas.costa@sou.unifal-mg.edu.br>
2021-04-13 11:02:49 -0300

    Inserindo uma nova Categoria com Post

--- src/main/java/com/shevalucas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/shevalucas/cursomc/resources/CategoriaResource.java
@@ -5,10 +5,10 @@ import com.shevalucas.cursomc.domain.Categoria;
 import com.shevalucas.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -23,5 +23,14 @@ public class CategoriaResource {
         Categoria obj = service.find(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity <Void> insert(@RequestBody Categoria obj){
+        System.out.println("teste");
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
