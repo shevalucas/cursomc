19f199d2469cf2c6691ef8b5ad16eb5e058ed9f9
shevalucas <lucas.costa@sou.unifal-mg.edu.br>
2021-04-16 10:51:30 -0300

    Validação customizado, email não repetiro

--- src/main/java/com/shevalucas/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/shevalucas/cursomc/repositories/ClienteRepository.java
@@ -4,8 +4,12 @@ import com.shevalucas.cursomc.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+    @Transactional // transação de somente leitura
+    Cliente findByEmail(String email);// Só de fazer isso no repository, ele já busca o email
 
 }
